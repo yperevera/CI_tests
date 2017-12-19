df1a88298c3e2a14e825d41a0ee8e15c2b907d72
yperevera <y.perevera@gmail.com>
2017-12-19 21:55:35 +0200

    changed .gitignore file and add shell script for jenkins job creation

---
+++ sh/create_new_job.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo '#### Create Virtual Environment ####'
+VIRTUAL_ENV_NAME='virtual-environment'
+virtualenv $VIRTUAL_ENV_NAME
+
+
+echo '#### Activate Virtual Environment ####'
+source $VIRTUAL_ENV_NAME/Scripts/activate
+
+
+echo '#### Install requirements ####'
+pip install -r ../requirements.txt
+
+
+echo '#### Run tests ####'
+py.test --alluredir=../../allure-results ../test_main_page.py  
+
+ 
+echo '### deactivate virtual environment ###'
+deactivate
